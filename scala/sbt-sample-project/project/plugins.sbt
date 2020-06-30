57d4173bd2ae4f59e883af699c11c198115ad712
Bhavya Aggarwal <bhavya@knoldus.com>
2020-06-30 22:40:50 +0530

    WIP:Added a sample maven project for all quality metrics plugin settings; added settings for scalastyle;scoverage and pmd:cpd

---
+++ scala/sbt-sample-project/project/plugins.sbt
@@ -0,0 +1,7 @@
+addSbtPlugin("org.scoverage" % "sbt-scoverage" % "1.5.1")
+addSbtPlugin("org.scalastyle" %% "scalastyle-sbt-plugin" % "1.0.0")
+addSbtPlugin("org.scalameta" % "sbt-scalafmt" % "2.4.0") // "2.4.0" is just sbt plugin version
+addSbtPlugin("com.sksamuel.scapegoat" %% "sbt-scapegoat" % "1.1.0")
+addSbtPlugin("com.github.sbt" % "sbt-cpd" % "2.0.0")
+addSbtPlugin("nl.codestar" % "sbt-findsecbugs" % "0.15")
+
---
+++ scala/sbt-sample-project/src/main/scala/com/knoldus/styleguide/ExampleLogic.scala
@@ -0,0 +1,24 @@
+package com.knoldus.styleguide
+
+object ExampleLogic {
+
+  def getResult = {
+    val randomResul1 = 13
+    val Randomoutput = null
+    randomResul1 + Randomoutput
+  }
+
+  //Duplicating everything for sbt-cpd
+  def getResult1 = {
+    val randomResul1 = 13
+    val Randomoutput = null
+    randomResul1 + Randomoutput
+  }
+
+  def main (args: Array[String]) = {
+    //Potential FindSecurity Bug
+    val file = new java.io.File(args(0))
+    val ANOTHERna = 2
+    println(s"The file contents are:${file.setReadable(false)}")
+  }
+}
